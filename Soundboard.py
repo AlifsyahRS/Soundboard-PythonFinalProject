fc2e917567f70237aad0010735a1c4a7587c28cd
AlifsyahRS <71362235+AlifsyahRS@users.noreply.github.com>
2020-12-20 20:06:14 +0700

    Created a preference button for each play button

--- Soundboard.py
+++ Soundboard.py
@@ -1,41 +1,59 @@
-
-# Used for creating a file explorer to select a file.
 import tkinter as tk
 from playsound import playsound
+# Used for creating a file explorer to select a file.
 from tkinter import filedialog
 
 
 class Soundboard():
-    def __init__(self, button_num):
+    def __init__(self, button_num, master):
         self.button_num = int(button_num)
         self.file = None
         self.button_label = f"Audio {self.button_num}"
+        self.master = master
 
     def setFile(self):
         self.file = filedialog.askopenfilename(
-            filetypes=[("Mp3 Files", "*.mp3")]).replace(" ", "%20")
+            filetypes=[("Mp3 Files", "*.mp3"), ("Uncompressed Audio Files", "*.wav")]).replace(" ", "%20")
         # When executed, this function will create a file explorer wherein the user can choose top open the audio files the choose.
         # Replace is needed because the playsound module cannot reach a directory which contains a space character in its name
 
-    def getFileDir(self):
-        return self.file
-
-    def setButtonLabel(self, new_label):
-        self.button_label = new_label
-
     def playSound(self):
         if self.file == None:
+            # Nothing happens if no file is chosen.
             pass
         else:
+            # playsound() plays the audio file
             playsound(self.file)
 
-    def packButton(self, master):
-        frame = tk.Frame(master)
-        frame.pack()
+    def setButtonLabel(self):
+        rename_window = tk.Toplevel(self.master)
+        rename_str = tk.StringVar()  # Storage for user input
+        rename_entry = tk.Entry(rename_window, bd=5)
+        rename_entry.pack(side=tk.LEFT)
+        rename_button = tk.Button(
+            rename_window, text="Enter", bd=5, command=rename_entry.get)
+        rename_button.pack(side=tk.RIGHT)
+        rename_str.set(rename_button)
+        self.button_label = rename_str.get()
+
+    def buttonPreferences(self):
+        new_window = tk.Toplevel(self.master)
+        setfile_button = tk.Button(
+            new_window, text="Swap Current\nAudio File", width=10, height=5, command=self.setFile)
+        setfile_button.pack()
+        change_buttonlabel = tk.Button(
+            new_window, text="Rename Button\n(WIP)", width=10, height=5, command=self.setButtonLabel)
+        change_buttonlabel.pack()
+
+    def packButton(self):
+        # I used a frame object to help organize the buttons
+        frame = tk.Frame(self.master)
+        frame.pack()  # .pack() puts objects into the master window of the program
+        # Button object creates buttons
         play_button = tk.Button(frame, text=self.button_label, width=10,
                                 height=5, command=self.playSound)
+        # side parameter puts the button in a specific position relative to the frame
         play_button.pack(side=tk.LEFT)
-        changefile_button = tk.Button(
-            frame, text="^", width=3, height=3, command=self.setFile)
-        changefile_button.pack(side=tk.RIGHT)
-        play_button.pack()
+        button_preferences = tk.Button(
+            frame, text="^", width=3, height=3, command=self.buttonPreferences)
+        button_preferences.pack(side=tk.RIGHT)
--- main.py
+++ main.py
@@ -1,37 +1,36 @@
 from Soundboard import Soundboard
 import tkinter as tk
 
-Button_1 = Soundboard(1)
-Button_2 = Soundboard(2)
-Button_3 = Soundboard(3)
-Button_4 = Soundboard(4)
-Button_5 = Soundboard(5)
-Button_6 = Soundboard(6)
-
 
 def menubar(master):
     menu = tk.Menu(master)
-    master.config(menu=menu)
-    settingsmenu = tk.Menu(menu)
-    helpmenu = tk.Menu(menu)
-    menu.add_cascade(label='Settings', menu=settingsmenu)
-    # For settings menubar
-    settingsmenu.add_command(label="Change Volume (WIP)")
-    settingsmenu.add_command(label="Close Program", command=master.quit)
-    # For help menubar
-    menu.add_cascade(label='Help', menu=helpmenu)
-    helpmenu.add_command(label='How to Use the Program')
+    master.config(menu=menu)  # Creates a custom menu bar
+    preferences_menu = tk.Menu(menu)
+    help_menu = tk.Menu(menu)
+    # Code for preferences menubar
+    menu.add_cascade(label='Preferences', menu=preferences_menu)
+    preferences_menu.add_command(label="Adjust")
+    preferences_menu.add_command(label="Close Program", command=master.quit)
+    # Code for help menubar
+    menu.add_cascade(label='Help', menu=help_menu)
+    help_menu.add_command(label='How to Use the Program')
 
 
 def main():
-    master = tk.Tk()
+    master = tk.Tk()  # Creates the GUI for the program
     menubar(master)
-    Button_1.packButton(master)
-    Button_2.packButton(master)
-    Button_3.packButton(master)
-    Button_4.packButton(master)
-    Button_5.packButton(master)
-    Button_6.packButton(master)
+    Button_1 = Soundboard(1, master)
+    Button_2 = Soundboard(2, master)
+    Button_3 = Soundboard(3, master)
+    Button_4 = Soundboard(4, master)
+    Button_5 = Soundboard(5, master)
+    Button_6 = Soundboard(6, master)
+    Button_1.packButton()
+    Button_2.packButton()
+    Button_3.packButton()
+    Button_4.packButton()
+    Button_5.packButton()
+    Button_6.packButton()
     master.mainloop()
 
 
