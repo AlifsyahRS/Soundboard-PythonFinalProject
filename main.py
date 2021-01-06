3210e6b2f279bbced48c5c370035d9717783f128
AlifsyahRS <71362235+AlifsyahRS@users.noreply.github.com>
2021-01-06 14:10:26 +0800

    Removed the custom menubar

--- main.py
+++ main.py
@@ -2,24 +2,9 @@ from Soundboard import Soundboard
 import tkinter as tk
 
 
-def menubar(master):
-    menu = tk.Menu(master)
-    master.config(menu=menu)  # Creates a custom menu bar
-    preferences_menu = tk.Menu(menu)
-    help_menu = tk.Menu(menu)
-    # Code for preferences menubar
-    menu.add_cascade(label='Preferences', menu=preferences_menu)
-    preferences_menu.add_command(label="Adjust")
-    preferences_menu.add_command(label="Close Program", command=master.quit)
-    # Code for help menubar
-    menu.add_cascade(label='Help', menu=help_menu)
-    help_menu.add_command(label='How to Use the Program')
-
-
 def main():
     master = tk.Tk()  # Creates the GUI for the program
     master.title("Soundboard")
-    menubar(master)
     Button_1 = Soundboard(1, master)
     Button_2 = Soundboard(2, master)
     Button_3 = Soundboard(3, master)
