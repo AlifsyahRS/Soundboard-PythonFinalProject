296887bb714ff793d7c3941d8ca787cde13137b7
AlifsyahRS <71362235+AlifsyahRS@users.noreply.github.com>
2021-01-08 14:44:46 +0800

    Added code comments.

--- Soundboard.py
+++ Soundboard.py
@@ -9,6 +9,7 @@ class Soundboard():
     def __init__(self, button_num, master):
         self.button_num = int(button_num)
         self.file = None
+        # Since I can only initialize the main window in the main file, I need a placeholder variable to act as the main window.
         self.master = master
         self.frame = tk.Frame(master)
         self.play_button = tk.Button(self.frame, text=f"Audio {self.button_num}", width=10,
@@ -16,12 +17,6 @@ class Soundboard():
         self.button_preferences = tk.Button(
             self.frame, text="*", width=3, height=3, command=self.buttonPreferences)
         self.rename_str = tk.StringVar()
-
-    def setFile(self):
-        self.file = filedialog.askopenfilename(
-            filetypes=[("Mp3 Files", "*.mp3"), ("Uncompressed Audio Files", "*.wav")]).replace(" ", "%20")
-        # When executed, this function will create a file explorer wherein the user can choose top open the audio files the choose.
-        # Replace is needed because the playsound module cannot reach a directory which contains a space character in its name
 
     def playSound(self):
         if self.file == None:
@@ -31,11 +26,27 @@ class Soundboard():
             # playsound() plays the audio file
             playsound(self.file)
 
-    def getNewButtonText(self):
-        # Changes the play button label into the value of rename_str
-        self.play_button.configure(text=self.rename_str.get())
+    def buttonPreferences(self):
+        # Creating a new window
+        new_window = tk.Toplevel(self.master)
+        new_window.title(f"Preferences for Button {self.button_num}")
+        # setfile_button executes the setFile function which opens the file directory
+        setfile_button = tk.Button(
+            new_window, text="Swap Current\nAudio File", width=10, height=5, command=self.setFile)
+        setfile_button.pack()
+        # change_buttonlabel initializes setButtonLabel which creates a new window.
+        change_buttonlabel = tk.Button(
+            new_window, text="Rename Button", width=10, height=5, command=self.setButtonLabel)
+        change_buttonlabel.pack()
+
+    def setFile(self):
+        self.file = filedialog.askopenfilename(
+            filetypes=[("Mp3 Files", "*.mp3"), ("Uncompressed Audio Files", "*.wav")]).replace(" ", "%20")
+        # When executed, this function will create a file explorer wherein the user can choose top open the audio files the choose.
+        # Replace is needed because the interpreter cannot reach a directory which contains a space character in its name
 
     def setButtonLabel(self):
+        # Toplevel creates a separate window from the main window.
         rename_window = tk.Toplevel(self.master)
         rename_window.title(f"Rename Play Button {self.button_num}")
         rename_entry = tk.Entry(rename_window, bd=5,
@@ -44,18 +55,13 @@ class Soundboard():
             rename_window, bd=5, text="Enter", command=self.getNewButtonText)  # Changes play button label on pressing enter button
         # Changes play button label on hitting enter
         rename_window.bind("<Return>", lambda getText: self.getNewButtonText())
+        # .pack() makes the respective object visible and interactable in the GUI
         rename_entry.pack(side=tk.LEFT)
         rename_button.pack(side=tk.RIGHT)
 
-    def buttonPreferences(self):
-        new_window = tk.Toplevel(self.master)
-        new_window.title(f"Preferences for Button {self.button_num}")
-        setfile_button = tk.Button(
-            new_window, text="Swap Current\nAudio File", width=10, height=5, command=self.setFile)
-        setfile_button.pack()
-        change_buttonlabel = tk.Button(
-            new_window, text="Rename Button", width=10, height=5, command=self.setButtonLabel)
-        change_buttonlabel.pack()
+    def getNewButtonText(self):
+        # Changes the play button label into the value of rename_str
+        self.play_button.configure(text=self.rename_str.get())
 
     def packButtons(self):
         self.master.bind(
